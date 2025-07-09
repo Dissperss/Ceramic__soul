fc634d737b3d1351f465aa39c5330bfbea5136fb
Dissperss <subbotin.vladimir2018@ya.ru>
2025-07-09 13:27:21 +0300

    Slider added, JS started

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
 	build: {
 		rollupOptions: {
 			input: {
