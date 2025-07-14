c70399a910af9d8f1aa32cbeded5cbcb36075d55
Putintsev Nikita <nputintsev@neoflex.ru>
2025-07-14 16:10:14 +0300

    fix: fixed vite hot reload failure

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
-	server: {
-		watch: {
-			usePolling: true,
-		},
-	},
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				catalog: resolve(__dirname, "catalog.html"),
-			},
-		},
-	},
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                logger: { warn: () => { } } // полностью отключает предупреждения Sass
+            }
+        }
+    }
 });
 
