de5d971d4b9f161bfc048c09cd2567f84385a916
Dissperss <subbotin.vladimir2018@ya.ru>
2025-07-18 11:39:35 +0300

    Sending form from footer and animations for some elements

--- src/js/script.js
+++ src/js/script.js
@@ -172,19 +172,34 @@ try {
                 .parentElement.querySelector(".email-error-message"),
         }
     );
-    validatorFooter.addField(
-        "#footer__checkbox",
-        [
+    validatorFooter
+        .addField(
+            "#footer__checkbox",
+            [
+                {
+                    rule: "required",
+                },
+            ],
             {
-                rule: "required",
-            },
-        ],
-        {
-            errorsContainer: document
-                .querySelector("#footer__checkbox")
-                .parentElement.parentElement.querySelector(
-                    ".checkbox-error-message"
-                ),
-        }
-    );
+                errorsContainer: document
+                    .querySelector("#footer__checkbox")
+                    .parentElement.parentElement.querySelector(
+                        ".checkbox-error-message"
+                    ),
+            }
+        )
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("Success", data);
+                    form.reset();
+                });
+        });
 } catch (e) {}
