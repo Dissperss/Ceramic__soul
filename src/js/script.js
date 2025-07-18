d36277384815146b3a6c060797bf63de7a6591c0
Dissperss <subbotin.vladimir2018@ya.ru>
2025-07-18 10:46:26 +0300

    Sending forms

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,32 @@
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
+import JustValidate from "just-validate";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
 import "../sass/libs/fontello.css";
-import "../sass/libs/modern-normalize.min.css"
+import "../sass/libs/modern-normalize.min.css";
 
 import "../sass/style.scss";
 // import "/src/sass/style.scss";
 
-
 const burger = document.querySelector(".burger"),
-	close = document.querySelector(".header__menu-close"),
-	menu = document.querySelector(".header__menu");
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
 
 burger.addEventListener("click", () => {
-	menu.classList.add("header__menu_active");
-	document.body.style.overflow = "hidden";
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
 });
 
 close.addEventListener("click", () => {
-	menu.classList.remove("header__menu_active");
-	document.body.style.overflow = "";
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
 });
 
 try {
@@ -42,7 +42,7 @@ try {
             prevEl: ".icon-left-open",
         },
         breakpoints: {
-        // when window width is >= 1200px
+            // when window width is >= 1200px
             1200: {
                 slidesPerView: 3,
                 spaceBetween: 5,
@@ -57,24 +57,134 @@ try {
 } catch (e) {}
 
 try {
-	const tabs = document.querySelectorAll(".catalog__tab");
-	const contents = document.querySelectorAll(".catalog__content-item");
+    const tabs = document.querySelectorAll(".catalog__tab");
+    const contents = document.querySelectorAll(".catalog__content-item");
 
-	tabs.forEach((tab, index) => {
-		tab.addEventListener("click", () => {
-			// Удаляем активный класс у всех табов и контента
-			tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
-			contents.forEach((c) => (c.style.display = "none"));
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            // Удаляем активный класс у всех табов и контента
+            tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+            contents.forEach((c) => (c.style.display = "none"));
 
-			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
-			tab.classList.add("catalog__tab_active");
-			contents[index].style.display = "flex";
-		});
-	});
+            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+            tab.classList.add("catalog__tab_active");
+            contents[index].style.display = "flex";
+        });
+    });
 
-	// Показываем первый контент при загрузке
-	contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+    // Показываем первый контент при загрузке
+    contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
 } catch (e) {}
 
 // Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
 
+try {
+    const validatorTouch = new JustValidate(".touch__form");
+
+    validatorTouch.addField("#name", [
+        {
+            rule: "required",
+            errorMessage: "Please fill the name",
+        },
+        {
+            rule: "minLength",
+            value: 2,
+            errorMessage: "Min 2 char!",
+        },
+    ]);
+    validatorTouch.addField("#email", [
+        {
+            rule: "required",
+            errorMessage: "Please fill the email",
+        },
+        {
+            rule: "email",
+            errorMessage: "Incorrect form of email",
+        },
+    ]);
+    validatorTouch.addField(
+        "#question",
+        [
+            {
+                rule: "required",
+            },
+            {
+                rule: "minLength",
+                value: 5,
+            },
+        ],
+        {
+            errorsContainer: document
+                .querySelector("#question")
+                .parentElement.querySelector(".error-message"),
+        }
+    );
+    validatorTouch
+        .addField(
+            "#checkbox",
+            [
+                {
+                    rule: "required",
+                },
+            ],
+            {
+                errorsContainer: document
+                    .querySelector("#checkbox")
+                    .parentElement.parentElement.querySelector(
+                        ".checkbox-error-message"
+                    ),
+            }
+        )
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("Success", data);
+                    form.reset();
+                });
+        });
+} catch (e) {}
+
+try {
+    const validatorFooter = new JustValidate(".footer__form");
+
+    validatorFooter.addField(
+        "#footer__email",
+        [
+            {
+                rule: "required",
+                errorMessage: "Please fill the email",
+            },
+            {
+                rule: "email",
+                errorMessage: "Incorrect form of email",
+            },
+        ],
+        {
+            errorsContainer: document
+                .querySelector("#footer__email")
+                .parentElement.querySelector(".email-error-message"),
+        }
+    );
+    validatorFooter.addField(
+        "#footer__checkbox",
+        [
+            {
+                rule: "required",
+            },
+        ],
+        {
+            errorsContainer: document
+                .querySelector("#footer__checkbox")
+                .parentElement.parentElement.querySelector(
+                    ".checkbox-error-message"
+                ),
+        }
+    );
+} catch (e) {}
